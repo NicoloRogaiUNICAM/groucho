734978172114f65d9fa734558e1c2f2df9855324
NicoloRogaiUNICAM <nicolo.rogai@studenti.unicam.it>
2020-03-22 18:54:19 +0100

    Spring boot init

---
+++ groucho-lsh-rest/src/main/java/it/cnr/iasi/saks/groucho/groucholshrest/GrouchoLshRestApplication.java
@@ -0,0 +1,13 @@
+package it.cnr.iasi.saks.groucho.groucholshrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouchoLshRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouchoLshRestApplication.class, args);
+	}
+
+}
---
+++ groucho-lsh-rest/src/test/java/it/cnr/iasi/saks/groucho/groucholshrest/GrouchoLshRestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cnr.iasi.saks.groucho.groucholshrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouchoLshRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
